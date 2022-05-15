bcd86e9a7154f14be4ac0f77141770fad5b79747
wgywmm <wgywmm@gmail.com>
2022-05-16 01:14:50 +0800

    Add files via upload

---
+++ qrbtf/precache-manifest.ea241725e89b26f5bf8ca4370b5c597d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20c70ef5222c22af7559824715cab91",
+    "url": "/s1/qrbtf/index.html"
+  },
+  {
+    "revision": "eb3a1d4f56d1c6cf68f8",
+    "url": "/s1/qrbtf/static/css/main.d6db381f.chunk.css"
+  },
+  {
+    "revision": "b3ee111135a03ff54298",
+    "url": "/s1/qrbtf/static/js/2.2a9a44ae.chunk.js"
+  },
+  {
+    "revision": "c2dc3a0da7ac76b30a247ebb412c0352",
+    "url": "/s1/qrbtf/static/js/2.2a9a44ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3a1d4f56d1c6cf68f8",
+    "url": "/s1/qrbtf/static/js/main.623b2f92.chunk.js"
+  },
+  {
+    "revision": "958a841a12b70a0f1b39",
+    "url": "/s1/qrbtf/static/js/runtime-main.63a3e637.js"
+  },
+  {
+    "revision": "5e638c9a6be0d68e9a635ad74cafdb22",
+    "url": "/s1/qrbtf/static/media/Futura.5e638c9a.ttc"
+  }
+]);
